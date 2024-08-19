7931bb5d8219e5a6b2dffadfa5fdeaa5e706d4f7
sunny-yadav <sunny9405683@gmail.com>
2024-08-19 17:28:42 +0530

    added liner-pulse animaiton

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      animation:{
-        'scale-pulse' : 'scalepulse 2s infinite'
+      animation: {
+        "scale-pulse": "scalepulse 2s infinite",
+        "liner-pulse": "linerPulse 1s infinite",
       },
-      keyframes : {
-        scalepulse : {
-          '0% , 100%' : { transfrom : 'scale(1)'},
-          '50%' : { transform : 'scale(1.2)'}
-        }
-      }
+      keyframes: {
+        scalepulse: {
+          "0% , 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.2)" },
+        },
+        linerPulse: {
+          "0%": { transform: "translateX(0)" },
+          "50%": { transform: "translateX(-10px)" }, // Moves left by 10px
+          "100%": { transform: "translateX(0)" },
+        },
+      },
     },
   },
   plugins: [],
-}
-
+};
