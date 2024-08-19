98cd6e73d70cb64e2f5d2be9e39748b3bdd6740d
sunny-yadav <sunny9405683@gmail.com>
2024-08-19 15:34:01 +0530

    configured the redux store and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
+import { Provider } from 'react-redux'
 import './App.css'
-import Features from './components/Features'
-import Card from './components/Card'
 import TermsPage from './components/TermsPage'
+import { createBrowserRouter, createRoutesFromElements , RouterProvider , Route } from 'react-router-dom'
+import {store} from './Redux/Store'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={ <TermsPage/>} />
+    </>
+  )
+)
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <TermsPage/>
-      {/* <Features/> */}
+      <Provider store={store}>
+        <RouterProvider router={router}/>
+      </Provider>
     </>
   )
 }
