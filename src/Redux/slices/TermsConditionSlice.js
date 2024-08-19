b64c86654e26c45c0be19ed4a21359347cb5aeba
sunny-yadav <sunny9405683@gmail.com>
2024-08-19 15:34:34 +0530

    created termslice and store

---
+++ src/Redux/slices/TermsConditionSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    checkBoxState : "",
+}
+
+export const termConditionslice = createSlice({
+    name:"TermCondition",
+    initialState,
+    reducers:{
+
+    }
+});
+
+export const {} = termConditionslice.actions;
+export default termConditionslice.reducer
