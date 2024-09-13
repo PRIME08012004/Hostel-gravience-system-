5619eaaeabbcc738dcb4bf9c81c8a02542584559
Omkar Shirsekar <113287668+PRIME08012004@users.noreply.github.com>
2024-09-13 11:45:53 +0530

    make changes in server

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg'); 
 const cors = require('cors'); 
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt'); // Add bcrypt
+const bcrypt = require('bcrypt'); 
 
 const app = express();
 const port = 5000;
@@ -30,7 +30,7 @@ app.post('/register', async (req, res) => {
   }
 
   try {
-    // Hash the password before storing it
+   
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const result = await pool.query(
