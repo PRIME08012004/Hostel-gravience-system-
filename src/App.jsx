dfb6b44743e4a7a87e65ba1c4d41710d4e5ff5f0
sunny-yadav <sunny9405683@gmail.com>
2024-09-15 11:27:51 +0530

    updated the import of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import { Provider } from 'react-redux'
 import './App.css'
-import TermsPage from './components/TermsPage'
+import TermsPage from './components/TermsConditionPage/TermsPage'
 import { createBrowserRouter, createRoutesFromElements , RouterProvider , Route } from 'react-router-dom'
 import {store} from './Redux/Store'
-import Login_signIn from './components/Login_signIn'
-import Login from './components/Login'
-import SignIn from './components/SignIn'
-import Navbar from './components/IntroPage/Navbar'
-import Features from './components/Features'
+import Login_signIn from './components/student_Login_SignIn/Login_signIn'
+import Login from './components/student_Login_SignIn/Login'
+import SignIn from './components/student_Login_SignIn/SignIn'
+import Features from './components/featuresPage/Features'
 import Complaint from './components/Complaint'
+import Index from './components/IntroPage/Index'
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={ <Navbar/>} />
+      <Route path="/" element={ <Index/>} />
       <Route path="/features" element={ <Features/>} />
-      <Route path="/RaiseComplaint" element={ <Complaint/>} />
       <Route path="/features/TermsPage" element={ <TermsPage/>} />
+      <Route path="/RaiseComplaint" element={ <Complaint/>} />
       <Route path="/Login_signIn" element={<Login_signIn />}>
         <Route path="/Login_signIn/" element={<Login/>} />
         <Route path="/Login_signIn/SignIn" element={<SignIn/>} />
