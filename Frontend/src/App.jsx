f4aa31ad3eda9b4974e04155406af140be3e1099
sunny-yadav <sunny9405683@gmail.com>
2024-10-19 22:21:08 +0530

    Renamed folder from 'student_Login_SignIn to 'Student_forms' for clarity

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { Provider } from 'react-redux'
 import './App.css'
 import TermsPage from './components/TermsConditionPage/TermsPage'
-import { createBrowserRouter, createRoutesFromElements , RouterProvider , Route } from 'react-router-dom'
-import {store} from './Redux/Store'
-import Login_signIn from './components/student_Login_SignIn/Login_signIn'
-import Login from './components/student_Login_SignIn/Login'
-import SignIn from './components/student_Login_SignIn/SignIn'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
+import { store } from './Redux/Store'
+import Login_signIn from './components/Student_forms/Login_signIn'
+import Login from './components/Student_forms/Login'
+import SignIn from './components/Student_forms/SignIn'
 import Features from './components/featuresPage/Features'
 import Complaint from './components/Complaint'
 import Index from './components/IntroPage/Index'
@@ -14,15 +14,15 @@ import Index from './components/IntroPage/Index'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={ <Index/>} />
-      <Route path="/features" element={ <Features/>} />
-      <Route path="/features/TermsPage" element={ <TermsPage/>} />
-      <Route path="/RaiseComplaint" element={ <Complaint/>} />
+      <Route path="/" element={<Index />} />
+      <Route path="/features" element={<Features />} />
+      <Route path="/features/TermsPage" element={<TermsPage />} />
+      <Route path="/RaiseComplaint" element={<Complaint />} />
       <Route path="/Login_signIn" element={<Login_signIn />}>
-        <Route path="/Login_signIn/" element={<Login/>} />
-        <Route path="/Login_signIn/SignIn" element={<SignIn/>} />
+        <Route path="/Login_signIn/" element={<Login />} />
+        <Route path="/Login_signIn/SignIn" element={<SignIn />} />
       </Route>
-    
+
     </>
   )
 )
@@ -33,7 +33,7 @@ function App() {
   return (
     <>
       <Provider store={store}>
-        <RouterProvider router={router}/>
+        <RouterProvider router={router} />
       </Provider>
     </>
   )
