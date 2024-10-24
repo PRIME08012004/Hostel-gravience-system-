f4546f8b528f87da8864cd20bce286fea3805544
sunny-yadav <sunny9405683@gmail.com>
2024-10-24 20:21:29 +0530

    added bounce-once animation

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       animation: {
         "scale-pulse": "scalepulse 2s infinite",
         "liner-pulse": "linerPulse 1s infinite",
+        "bounce-once": "bounceOnce 0.3s ease-in-out"
       },
       keyframes: {
         scalepulse: {
@@ -17,6 +18,11 @@ export default {
           "50%": { transform: "translateX(-10px)" }, // Moves left by 10px
           "100%": { transform: "translateX(0)" },
         },
+        bounceOnce : {
+          "0%" : {transform: "translateY(0)"},
+          "50%" : {transform: "translateY(-20px)"},
+          "100%" : {transform: "translateY(0)"}
+        }
       },
       colors: {
         'first-color': 'var(--first-color)', 
